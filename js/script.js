821414c584cf407fcad64e1793b1c26eaccc7ba8
Eason Chen <eason.yc.chen@fubon.com>
2020-10-28 13:37:28 +0800

    串接動畫路徑

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(function () {
     $(".js-mobileShare-toggle").click(function () {
         $(".mobileShare-content").slideToggle(200);
     });
+
+    $('.js-include-Animate').load("https://arx827.github.io/vue_animate_movie/");
 });
 // loading
 document.addEventListener("DOMContentLoaded", function () {
